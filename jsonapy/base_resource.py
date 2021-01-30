76dfa7743e08ca438c00f6e4580288181615ef42
Arkelis <guillaume.fayard@pycolore.fr>
2021-01-30 01:16:58 +0100

    Simpler metaclass, use __init_subclass__

--- jsonapy/base_resource.py
+++ jsonapy/base_resource.py
@@ -1,5 +1,8 @@
 import json
-from collections import Iterable
+from abc import ABC
+from abc import ABCMeta
+from typing import Iterable
+from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import Literal
@@ -10,41 +13,84 @@ from typing import Union
 from jsonapy import utils
 
 
-class BaseResourceMeta(type):
-    def __new__(mcs, name, bases, namespace):  # noqa C901
-        cls = super().__new__(mcs, name, bases, namespace)
+class _BaseResource:
+    """Private base class of BaseResource containing some special utilitarian methods."""
+
+    __annotations__ = {}
+
+    def __init_subclass__(cls, **kwargs):
+        cls.__annotations__ = cls.__fields_types__ = {
+            name: type_
+            for klass in cls.mro()
+            if issubclass(klass, _BaseResource)
+            for (name, type_) in klass.__annotations__.items()
+        }
 
         annotations_items = cls.__annotations__.items()
-        cls.__fields_types__ = cls.__annotations__
         cls.__atomic_fields_set__ = {
             name
             for name, type_ in annotations_items
-            if not issubclass(type_, BaseResource)
+            if not issubclass(type_, _BaseResource)
         }
         cls.__relationships_fields_set__ = {
             name
             for name, type_ in annotations_items
-            if issubclass(type_, BaseResource)
+            if issubclass(type_, _BaseResource)
         }
+
+
+class BaseResourceMeta(type):
+    def __new__(mcs, name, bases, namespace):
+        try:
+            meta = namespace.pop("Meta").__dict__
+        except KeyError:
+            meta = {}
+
+        cls = super().__new__(mcs, name, bases, namespace)
+
+        cls.__is_abstract__ = meta.get("is_abstract", False)
+        cls.__resource_name__ = meta.get("resource_name", cls.__name__)
+
+        if (
+            not cls.__is_abstract__
+            and not hasattr(cls, "id")
+            and "id" not in cls.__annotations__
+        ):
+            raise AttributeError("A Resource must have an 'id' attribute or property.")
 
         return cls
 
 
-class BaseResource(metaclass=BaseResourceMeta):
+class BaseResource(_BaseResource, metaclass=BaseResourceMeta):
     if TYPE_CHECKING:
         # for IDE
         __fields_types__: Dict[str, type]
         __atomic_fields_set__: Set[str]
         __relationships_fields_set__: Set[str]
+        __resource_name__: str
 
-    @property
-    def id(self):
-        raise NotImplementedError
+        # must be provided by subclasses
+        id: Any
 
     class Meta:
-        resource_name: str
+        is_abstract = True
 
-    def jsonapi_dict(self, required_attributes, links, relationships):
+    def __init__(self, **kwargs):
+        errors = []
+        missing_arguments = set(self.__fields_types__) - set(kwargs)
+        if missing_arguments:
+            errors.extend(f"    Missing argument: '{arg}'" for arg in missing_arguments)
+        if errors:
+            raise ValueError("\n" + "\n".join(errors))
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def jsonapi_dict(
+        self,
+        required_attributes: Union[Iterable[str], Literal["__all__"]],
+        links: Dict,
+        relationships: Dict,
+    ):
         """Dump the object as JSON in compliance with JSON:API specification.
 
         Parameters
@@ -71,7 +117,7 @@ class BaseResource(metaclass=BaseResourceMeta):
             }
         """
         data = {
-            "type": self.Meta.resource_name,
+            "type": self.__resource_name__,
             "id": self.id,
             "attributes": self.filtered_attributes(required_attributes),
         }
@@ -82,26 +128,39 @@ class BaseResource(metaclass=BaseResourceMeta):
             data["relationships"] = self.formatted_relationships(relationships)
         return data
 
-    def filtered_attributes(self, required_attributes: Union[Iterable, Literal["__all__"]]):
+    def filtered_attributes(
+        self, required_attributes: Union[Iterable, Literal["__all__"]]
+    ) -> Dict:
         if required_attributes == "__all__":
             required_attributes = self.__atomic_fields_set__
+        unexpected_attributes = set(required_attributes) - self.__atomic_fields_set__
+        if unexpected_attributes:
+            raise ValueError(
+                "\n"
+                + "\n".join(
+                    f"Unexpected required attribute: '{name}'"
+                    for name in unexpected_attributes
+                )
+            )
         return {
             utils.snake_to_camel_case(k): v
             for (k, v) in self.__dict__.items()
             if k in required_attributes
         }
 
-    def validate_relationships(self, relationships):
+    def validate_relationships(self, relationships: Dict) -> None:
         errors = []
         for name, rel_dict in relationships.items():
             if name not in self.__relationships_fields_set__:
                 errors.append(f"'{name}' is not a valid relationship.")
             if (rel_dict.get("links") is None and rel_dict.get("fields")) is None:
-                errors.append(f"You must provide at least links or fields for the '{name}' relationship.")
+                errors.append(
+                    f"You must provide at least links or fields for the '{name}' relationship."
+                )
         if errors:
             raise ValueError("\n".join(errors))
 
-    def formatted_relationships(self, relationships):
+    def formatted_relationships(self, relationships: Dict) -> Dict:
         relationships_dict = {}
         for name, rel_payload in relationships.items():
             related_object: BaseResource = self.__dict__[name]
@@ -110,11 +169,21 @@ class BaseResource(metaclass=BaseResourceMeta):
                 continue
             rel_data = {}
             if rel_payload.get("fields") is not None:
-                rel_data["data"] = related_object.filtered_attributes(rel_payload["fields"])
+                rel_data["data"] = related_object.filtered_attributes(
+                    rel_payload["fields"]
+                )
             if rel_payload.get("links") is not None:
                 rel_data["links"] = rel_payload["links"]
             relationships_dict[utils.snake_to_camel_case(name)] = rel_data
         return relationships_dict
 
-    def dump(self, required_attributes, links, relationships, dump_function: Callable[[Dict], str] = json.dumps) -> str:
-        return dump_function(self.jsonapi_dict(required_attributes, links, relationships))
+    def dump(
+        self,
+        required_attributes: Union[Iterable[str], Literal["__all__"]],
+        links: Dict,
+        relationships: Dict,
+        dump_function: Callable[[Dict], str] = json.dumps,
+    ) -> str:
+        return dump_function(
+            self.jsonapi_dict(required_attributes, links, relationships)
+        )
