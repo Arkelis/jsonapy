4667a4013b535448f652aa39538d7f6f0631fde5
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2021-02-09 22:14:07 +0100

    Add dark theme for docs index.html

--- scripts/docs/docs.sh
+++ scripts/docs/docs.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 
 echo Removing current docs folder...
-rm -rf docs;
+rm -rf docs
 echo Building new docs...
-poetry run pdoc -t scripts/docs -o docs jsonapy;
+poetry run pdoc \
+    -e jsonapy=https://github.com/Arkelis/jsonapy/tree/master/jsonapy/ \
+    -t scripts/docs \
+    -o docs jsonapy
 echo Adding .nojekyll file...
-touch docs/.nojekyll;
+touch docs/.nojekyll
 echo Adding CNAME file...
 echo jsonapy.pycolore.fr > docs/CNAME
 echo Done.
